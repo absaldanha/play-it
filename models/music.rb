8a60944c0581c7ce04af686f95a1a02ed7ad460f
Alexandre Saldanha <alexandre.bsaldanha@gmail.com>
2015-07-04 19:05:25 -0300

    Added Music class.

--- models/music.rb
+++ models/music.rb
@@ -1,17 +1,18 @@
 ##
 # Class that represents a music
-
 class Music
-
+  ##
+  # Describes the path to the music in the OS
   attr_accessor :path
+
+  ##
+  # Describes the extracted features of the music
   attr_accessor :features
 
   ##
   # Creates a new music object with the given +path+ and +features+
-
   def initialize(path, features = nil)
     @path = path
     @features = features.nil? ? {} : features
   end
-
 end
