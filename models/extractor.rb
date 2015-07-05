90bb0048f95ddf00dc31dd351515f73bc3b74d00
Nathália Marques de Oliveira <nathipsl@hotmail.com>
2015-07-04 23:05:39 -0300

    Fix spec_helper, add samples and bin

---
+++ models/extractor.rb
@@ -0,0 +1,16 @@
+##
+# Extracts features from a music file.
+class Extractor
+  def initialize
+  end
+
+  def extract(path)
+    # Make output path
+    # Make command
+    # Run command
+    # Read contents of output
+    # Parse
+    # Destroy output file
+    # Return parsed output
+  end
+end
