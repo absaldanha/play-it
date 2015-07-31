ca5f44f06256ae5293f8ef1a2edb1141b0872533
Alexandre Saldanha <alexandre.bsaldanha@gmail.com>
2015-07-31 18:14:02 -0300

    Changed spec.

--- spec/models/music_spec.rb
+++ spec/models/music_spec.rb
@@ -4,34 +4,34 @@ RSpec.describe Music do
     let(:features_example) { { foo: 5.4, bar: 2 } }
 
     context 'when some features are given' do
-      subject(:music_with_features) { Music.new(path_example, features_example) }
+      subject { described_class.new(path_example, features_example) }
 
       it 'is an instance of Music' do
-        expect(subject).to be_a described_class
+        is_expected.to be_a described_class
       end
 
       it 'have the path of the music' do
-        expect(music_with_features.path).to eq(path_example)
+        expect(subject.path).to eq(path_example)
       end
 
       it 'have the features of the music' do
-        expect(music_with_features.features).to eq(features_example)
+        expect(subject.features).to eq(features_example)
       end
     end
 
     context 'when no features are given' do
-      subject(:music_without_features) { Music.new(path_example) }
+      subject { described_class.new(path_example) }
 
       it 'is an instance of Music' do
         expect(subject).to be_a described_class
       end
 
       it 'have the path of the music' do
-        expect(music_without_features.path).to eq(path_example)
+        expect(subject.path).to eq(path_example)
       end
 
       it 'dont have features' do
-        expect(music_without_features.features).to be_empty
+        expect(subject.features).to be_empty
       end
     end
   end
