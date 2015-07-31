ca70d2344ef15577b214301b8102b01914349a00
Alexandre Saldanha <alexandre.bsaldanha@gmail.com>
2015-07-31 17:55:16 -0300

    Changed folder structure. Added Rake. Changed Music specs.

--- lib/models/music.rb
+++ lib/models/music.rb
@@ -2,11 +2,11 @@
 # Class that represents a music
 class Music
   ##
-  # Describes the path to the music in the OS
+  # @return [String] the path to the music in the OS
   attr_accessor :path
 
   ##
-  # Describes the extracted features of the music
+  # @return [Hash] the extracted features of the music
   attr_accessor :features
 
   ##
