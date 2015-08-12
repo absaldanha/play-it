0ea8111963b59cbdbf8c8e30354b78c94d86e9a9
Alexandre Saldanha <alexandre.bsaldanha@gmail.com>
2015-08-12 14:14:21 -0300

    Doc for the Logger class

--- lib/models/logger.rb
+++ lib/models/logger.rb
@@ -1,10 +1,24 @@
+##
+# Class that manages the session events
 class Logger
+
+  ##
+  # @return [Array] the list of events of the session
   attr_reader :session_events
 
+  ##
+  # Creates a Logger object, which will manage the list of events
+  # of the session
+  #
+  # @return [Logger] the logger for the session
   def initialize
     @session_events = []
   end
 
+  ##
+  # Adds the new +event+ to the list
+  #
+  # @param event [Hash] the event to be added
   def log(event)
     return false unless event.is_a? Hash
     @session_events << event
